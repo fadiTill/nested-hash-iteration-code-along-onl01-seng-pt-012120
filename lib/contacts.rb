a7e666370d0a1a1b674779954d4381110bc41b0e
fadimata tillman <fadi.tillman@gmail.com>
2020-02-03 06:08:19 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -37,7 +37,9 @@ def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
-      binding.pry
+      if attribute == :favorite_ice_cream_flavors
+        binding.pry
+      end
     end
   end
 end# This is the array we will be passing into the remove_strawberry method
